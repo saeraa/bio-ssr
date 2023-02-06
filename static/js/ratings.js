72ac64e877c1c0c078137f321763eb57255d6523
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2023-02-06 16:57:20 +0100

    Changed rating from text to hats. Stole Sara's hats ;)

--- static/js/ratings.js
+++ static/js/ratings.js
@@ -2,15 +2,31 @@ async function getRating() {
     const id = window.location.href.slice(window.location.href.lastIndexOf("/") + 1);
     const res = await fetch(`/api/movies/${id}/rating`);
     const rating = await res.json();
-
-    const movieRating = document.querySelector(".movie-rating");
-    const movieRatingImg = document.querySelector(".movie-rating-img");
     
     if (rating.rating !== "API Error") {
-        movieRating.hidden = "";
-        movieRatingImg.hidden = "";
-        
-        movieRating.innerText = rating.rating;
+
+        for (let i=0; i<5; i++) {
+
+            let ul = document.querySelector(".movie-rating-hats");
+            let li = document.createElement("li");
+            
+            if (i < rating.rating) {
+            
+                if ( i == Math.floor(rating.rating)) {
+            
+                    li.classList.add("hat-halffill");
+            
+                } else {
+            
+                    li.classList.add("hat-fill")
+                }
+            } else {
+
+                li.classList.add("hat-nofill")
+            }
+
+            ul.appendChild(li);
+        }
     }
 };
 
