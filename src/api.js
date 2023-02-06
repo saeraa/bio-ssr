ef50a98697acc10570521271200dd92894d8d3a1
Vedad <vedadz@gmail.com>
2023-02-06 20:46:58 +0100

    Movie Details page: Write a review
    * made addReview in views/partials for layout.
    * made style for addReview
    * js for client and server side
    * changed width of grid-template to make the review section fit
    * included body-parser and node-fetch in package.json

--- src/api.js
+++ src/api.js
@@ -1,9 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { postReview } from "./addReviewServer.js";
 
 const apiRouter = express.Router();
 
 apiRouter.get("/movies/:id", (req, res) => {
-	res.send(`movie with id of ${req.params.id}`);
+  res.send(`movie with id of ${req.params.id}`);
 });
+
+apiRouter.post(
+  "/movies/:movieId/reviews",
+  bodyParser.json(),
+  async (req, res) => {
+    postReview(req, res);
+  }
+);
 
 export default apiRouter;
