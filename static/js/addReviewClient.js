91a39e8e68aca40d25acc92720741b532786d7f9
Vedad <vedadz@gmail.com>
2023-02-06 20:56:14 +0100

    Update addReviewClient.js
    didnt save the formatting.. just update with prettier

--- static/js/addReviewClient.js
+++ static/js/addReviewClient.js
@@ -2,12 +2,13 @@ const addReviewButton = document.querySelector(".add-review-button");
 let addReviewContent = document.querySelector(".add-review-content");
 
 addReviewButton.addEventListener("click", () => {
-addReviewButton.classList.toggle("clicked");
-addReviewContent.classList.toggle("show");
+  addReviewButton.classList.toggle("clicked");
+  addReviewContent.classList.toggle("show");
 });
 
-
-document.querySelector(".submit-review").addEventListener("click", async (ev) => {
+document
+  .querySelector(".submit-review")
+  .addEventListener("click", async (ev) => {
     ev.preventDefault();
     const movieId = window.location.href.split("/").pop();
     const author = document.querySelectorAll("form > input")[0].value;
@@ -15,20 +16,34 @@ document.querySelector(".submit-review").addEventListener("click", async (ev) =>
     const comment = document.querySelector("textarea").value;
     addReviewContent.innerHTML = "";
     const reviewDone = document.createElement("h2");
-    reviewDone.textContent = "Thank you for the review, "+author+"!";
+    reviewDone.textContent = "Thank you for the review, " + author + "!";
     reviewDone.style.textAlign = "center";
     addReviewContent.appendChild(reviewDone);
-    setTimeout(()=>{
-    addReviewContent.classList.toggle("hide");
-    addReviewButton.classList.toggle("hide");
+    setTimeout(() => {
+      addReviewContent.classList.toggle("hide");
+      addReviewButton.classList.toggle("hide");
     }, 3000);
-    const response = await fetch(new Request('/api/movies/'+movieId+'/reviews', {
-        method: 'POST',
+    // i am going to use this variable in the second part of the assignment.
+    const response = await fetch(
+      new Request("/api/movies/" + movieId + "/reviews", {
+        method: "POST",
         headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({data: {comment: comment, rating: rating, author: author, verified: false, movie: movieId, createdAt: new Date(), updatedAt: new Date(), createdBy: author, updatedBy: author}})
-      }));
-    });
-    
+        body: JSON.stringify({
+          data: {
+            comment: comment,
+            rating: rating,
+            author: author,
+            verified: false,
+            movie: movieId,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            createdBy: author,
+            updatedBy: author,
+          },
+        }),
+      })
+    );
+  });
