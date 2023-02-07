cbea86dd52c961ca5c3a2956836a9315bc6e904d
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2023-02-07 14:06:44 +0100

    Fixed a bug in ratings.js, the declaration of ul was in the wrong place

--- static/js/ratings.js
+++ static/js/ratings.js
@@ -1,15 +1,17 @@
 async function getRating() {
     const id = window.location.href.slice(window.location.href.lastIndexOf("/") + 1);
+    
     const res = await fetch(`/api/movies/${id}/rating`);
     const data = await res.json();
     const rating = data.rating;
+
     const container = document.querySelector("#rating-container-element");
-
+    const ul = document.querySelector(".movie-rating-hats");
+    
     if (rating !== "API Error") {
-
+    
         for (let i=0; i<5; i++) {
-
-            let ul = document.querySelector(".movie-rating-hats");
+            
             let li = document.createElement("li");
             
             if (i < rating) {
@@ -29,7 +31,7 @@ async function getRating() {
 
             ul.appendChild(li);
         }
-
+        
         container.append(ul);
     }
 };
