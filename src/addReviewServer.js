5027d42e6e22a5af15c7092efb424faaf84b2d97
Vedad <vedadz@gmail.com>
2023-02-08 23:10:25 +0100

    Review validation - Vedad
    * added logic to server side for checking the request (author and rating)
    * throwing status 201 (Created) on sucess and 422 (Unprocessable Entity) on wrong input
    * changed client side to listend and respond to servers response
    * moved button outside form, made it bugging for some reason

--- src/addReviewServer.js
+++ src/addReviewServer.js
@@ -1,15 +1,64 @@
+import fetch from "node-fetch";
+
 const API_BASE = "https://plankton-app-xhkom.ondigitalocean.app/api/reviews";
 
 export async function postReview(req, res) {
-  const method = {
-    method: "POST",
-    body: JSON.stringify(req.body),
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  };
-  // i am going to use this variable in the second part of the assignment.
-  const response = await fetch(API_BASE, method);
-  res.send().status(200);
+  const review = req.body.data;
+  const reviewToValidate = validateReview(review);
+  if ((reviewToValidate.validated == true)) {
+    const method = {
+      method: "POST",
+      body: JSON.stringify(req.body),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    };
+    await fetch(API_BASE, method);
+    res.status(201).send({ data: reviewToValidate });
+  } else {
+    res.status(422).send({ data: reviewToValidate });
+  }
+}
+
+function validateReview(review) {
+  if (checkAuthor(review)) {
+    if (checkRating(review)) {
+      return {
+        validated: true,
+        message: "Thank you for the review, " + review.author + "!",
+      };
+    } else {
+      return {
+        validated: false,
+        message: "The rating needs to be between 0-5!",
+      };
+    }
+  } else if (!checkRating(review)) {
+    return {
+      validated: false,
+      message: "Please write a valid name and rating!",
+    };
+  } else {
+    return { validated: false, message: "Please write a valid name!" };
+  }
+}
+
+function checkAuthor(review) {
+  const author = review.author;
+  const regex = /[^a-zA-Z -]/;
+  if (author.match(regex)||author.length == 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function checkRating(review) {
+  const rating = review.rating;
+  if (0 <= rating && rating <= 5 && rating.length > 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
--- static/js/addReviewClient.js
+++ static/js/addReviewClient.js
@@ -1,49 +1,62 @@
-const addReviewButton = document.querySelector(".add-review-button");
-let addReviewContent = document.querySelector(".add-review-content");
+let addReviewButton = document.querySelector(".add-review-button");
+const addReviewContent = document.querySelector(".add-review-content");
 
 addReviewButton.addEventListener("click", () => {
   addReviewButton.classList.toggle("clicked");
   addReviewContent.classList.toggle("show");
 });
 
-document
-  .querySelector(".submit-review")
-  .addEventListener("click", async (ev) => {
-    ev.preventDefault();
-    const movieId = window.location.href.split("/").pop();
-    const author = document.querySelector("#name").value;
-    const rating = document.querySelector("#rating").value;
-    const comment = document.querySelector("#comment").value;
-    addReviewContent.innerHTML = "";
-    const reviewDone = document.createElement("h2");
-    reviewDone.textContent = "Thank you for the review, " + author + "!";
-    reviewDone.style.textAlign = "center";
-    addReviewContent.appendChild(reviewDone);
+const submitButton = document.querySelector(".submit-review");
+
+submitButton.addEventListener("click", postReviewHandler);
+
+async function postReviewHandler() {
+  const movieId = window.location.href.split("/").pop();
+  const author = document.querySelector("#name").value;
+  const rating = document.querySelector("#rating").value;
+  const comment = document.querySelector("#comment").value;
+  const template = addReviewContent.innerHTML;
+  const response = await fetch(
+    new Request("/api/movies/" + movieId + "/reviews", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data: {
+          comment: comment,
+          rating: rating,
+          author: author,
+          verified: false,
+          movie: movieId,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          createdBy: author,
+          updatedBy: author,
+        },
+      }),
+    })
+  );
+  const result = await response.json();
+
+  addReviewContent.innerHTML = "";
+  const reviewDone = document.createElement("h2");
+  reviewDone.textContent = result.data.message;
+  reviewDone.style.textAlign = "center";
+  addReviewContent.appendChild(reviewDone);
+
+  if (result.data.validated == true) {
     setTimeout(() => {
       addReviewContent.classList.toggle("hide");
       addReviewButton.classList.toggle("hide");
     }, 3000);
-    // i am going to use this variable in the second part of the assignment.
-    const response = await fetch(
-      new Request("/api/movies/" + movieId + "/reviews", {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          data: {
-            comment: comment,
-            rating: rating,
-            author: author,
-            verified: false,
-            movie: movieId,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            createdBy: author,
-            updatedBy: author,
-          },
-        }),
-      })
-    );
-  });
+  } else {
+    setTimeout(() => {
+      addReviewContent.innerHTML = template;
+      document
+        .querySelector(".add-review-content")
+        .childNodes[3].addEventListener("click", postReviewHandler);
+    }, 3000);
+  }
+}
