30a369be8d3a51266277cb5b01513ed3b623e989
Arash-SJPI22 <arash.eblaghian@lerniastudent.se>
2023-02-08 09:35:56 +0100

    Bug fixing

--- src/api.js
+++ src/api.js
@@ -4,48 +4,44 @@ import getMovieRating from "./getMovieRating.js";
 import { loadMovie } from "./movies.js";
 import { getReviewsForMovieWithId } from "./reviewList.js";
 import loadScreenings from "./loadScreenings.js";
-import {filterScreenings} from "./filterScreenings.js";
+import { filterScreenings } from "./filterScreenings.js";
 
 const apiRouter = express.Router();
 
 apiRouter.get("/movies/:id", (req, res) => {
-  res.send(`movie with id of ${req.params.id}`);
+	res.send(`movie with id of ${req.params.id}`);
 });
 
-apiRouter.post(
-  "/movies/:movieId/reviews",
- express.json(),
-  async (req, res) => {
-    postReview(req, res);
-  }
-);
+apiRouter.post("/movies/:movieId/reviews", express.json(), async (req, res) => {
+	postReview(req, res);
+});
 apiRouter.get("/movies/:id/rating", async (req, res) => {
-	const id = req.params.id; 
-	const movie = await loadMovie(id)
+	const id = req.params.id;
+	const movie = await loadMovie(id);
 	const rating = await getMovieRating(id, movie.data.attributes.imdbId);
 
 	if (rating.Response == "False") {
 		res.status(502).send({ rating: "API Error" });
 	} else {
-		res.status(200).send({ rating: rating.toFixed(1)});
+		res.status(200).send({ rating: rating.toFixed(1) });
 	}
+});
 
 apiRouter.get("/movies/:id/reviews", async (req, res) => {
 	const page = req.query.page;
 	const id = req.params.id;
 	const response = await getReviewsForMovieWithId(id, page);
 	res.status(200).json(response);
-
 });
 
-apiRouter.get("/screenings", async (req,res)=> {
+apiRouter.get("/screenings", async (req, res) => {
 	let data;
 	let filteredScreenings;
 	try {
 		data = await loadScreenings();
 		filteredScreenings = filterScreenings(data);
 		res.json(filteredScreenings);
-	} catch(error) {
+	} catch (error) {
 		res.status(error.response.status).json(error);
 	}
 });
